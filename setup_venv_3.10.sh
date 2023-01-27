ec4843e7c43a5e994fc825043848a5971ecde0b3
wgkim-bimatrix <dnsrl.bimatrix@gmail.com>
2023-01-27 15:48:42 +0900

    prepare env for 3.8

---
+++ setup_venv_3.10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod 777 ./linux/bi-venv/install-bi-venv.sh
+chmod 777 ./test.sh
+./linux/bi-venv/install-bi-venv.sh -b -p ./linux/bi-venv/venv/
+./linux/bi-venv/venv/bin/conda init bash
+source ~/.bashrc
+./linux/bi-venv/venv/bin/conda create -n mois -y python --offline
+
+echo "conda activate mois" >> ~/.bashrc
+source ~/.bashrc
