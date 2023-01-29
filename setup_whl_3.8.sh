7981f35ec551a26dee523da7dc7d306184b7ff5c
wgkim-bimatrix <dnsrl.bimatrix@gmail.com>
2023-01-30 08:59:06 +0900

    split install step and add pyodbc - add pyodbc - split steps installing venv and installing wheel

--- setup_whl_3.8.sh
+++ setup_whl_3.8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+conda activate mois3.8
 cat ./linux/wheelhouse/3.8/split/tensorflow_cpu-2.11.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl.tar.gz* | tar -zxvpf -
 mv tensorflow_cpu-2.11.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl ./linux/wheelhouse/3.8/
 pip3 install --no-index --find-links=linux/wheelhouse/3.8 -r requirements-offline-3.8.txt
-
