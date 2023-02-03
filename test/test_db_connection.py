4df0d805d5d42628c9441e783b8ef95118ca9793
wgkim-bimatrix <dnsrl.bimatrix@gmail.com>
2023-02-03 18:34:42 +0900

    add tibero env

--- test/test_db_connection.py
+++ test/test_db_connection.py
@@ -6,19 +6,19 @@ def test_db_connection():
 
 
 if __name__ == '__main__':
-    # conn = jp.connect(
-    #     'com.tmax.tibero.jdbc.TbDriver',
-    #     'jdbc:tibero:thin:@192.168.11.126:8629:tibero',
-    #     {'user': 'sys', 'password': 'matrix'},
-    #     './linux/lib/tibero6-jdbc.jar'
-    # )
-
     conn = jp.connect(
-        'com.tmax.tibero.jdbc.TbDriver',  # SAME
-        'jdbc:tibero:thin:@10.47.61.5:1579:DKRMS',
-        {'user': 'DRM', 'password': 'krms51mrd'},
+        'com.tmax.tibero.jdbc.TbDriver',
+        'jdbc:tibero:thin:@192.168.11.126:8629:tibero',
+        {'user': 'sys', 'password': 'matrix'},
         './linux/lib/tibero6-jdbc.jar'
     )
+
+    # conn = jp.connect(
+    #     'com.tmax.tibero.jdbc.TbDriver',  # SAME
+    #     'jdbc:tibero:thin:@10.47.61.5:1579:DKRMS',
+    #     {'user': 'DRM', 'password': 'krms51mrd'},
+    #     './linux/lib/tibero6-jdbc.jar'
+    # )
 
     try:
         with conn.cursor() as curs:
