c599e94ef636e3fb517eefae0579f1c5f6b00924
wgkim-bimatrix <dnsrl.bimatrix@gmail.com>
2023-02-06 09:04:58 +0900

    .

--- setup_venv.sh
+++ setup_venv.sh
@@ -2,6 +2,7 @@
 
 chmod 777 ./linux/bi-venv/install-bi-venv.sh
 chmod 777 *.sh
+
 ./linux/bi-venv/install-bi-venv.sh -b -p ./linux/bi-venv/venv/
 ./linux/bi-venv/venv/bin/conda init bash
 source ~/.bashrc
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+conda activate forecast
 pytest ./test
